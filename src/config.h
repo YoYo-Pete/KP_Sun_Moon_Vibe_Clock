c8e92eace3da6cbb83916357add3c4f5f80373dc
YoYo-Pete <yoyo.pete@gmail.com>
2013-05-13 11:26:10 -0400

    Changed for my lat/long at home

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE    XX.XXXX
-#define LONGITUDE -X.XXXXX
-#define TIMEZONE 0;
+#define LATITUDE    42.243821
+#define LONGITUDE -83.619626
+#define TIMEZONE -5;
 //NOTE: Change false to true if you want to enable the vibe function
-#define HOUR_VIBRATION false
+#define HOUR_VIBRATION true
